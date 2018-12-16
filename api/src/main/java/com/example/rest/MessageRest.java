781786c02b3c6c14de31addf72dd4b3ca9a4322a
Sliusar <slian01@ca.com>
2018-12-16 14:49:58 +0100

    complete Message API

--- api/src/main/java/com/example/rest/MessageRest.java
+++ api/src/main/java/com/example/rest/MessageRest.java
@@ -6,13 +6,23 @@ import java.util.List;
 
 import com.example.rest.entity.Message;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/")
+@RestController
+@RequestMapping("/message")
 public class MessageRest {
-    @RequestMapping("/messages")
-    public List<Message> messages() {
+    Logger logger = LoggerFactory.getLogger(MessageRest.class);
+
+    @RequestMapping(value="/", method=RequestMethod.GET)
+    public List<Message> getMessages() {
+        logger.info("Request all messages");
+
         List<Message> list = Arrays.asList(
             new Message("1", new Date()),
             new Message("2", new Date()),
@@ -20,5 +30,27 @@ public class MessageRest {
         );
 
         return list;
+    }
+
+    @RequestMapping(value="/{id}", method=RequestMethod.GET)
+    public Message getMessage(@PathVariable("id") String id) {
+        logger.info(String.format("Request a message with id: %s", id));
+
+        return new Message("1", new Date());
+    }
+
+    @RequestMapping(value="/", method=RequestMethod.POST)
+    public void addMessage(@RequestBody Message message) {
+        logger.info(String.format("Create new message with text: %s", message.getText()));
+    }
+    
+    @RequestMapping(value="/{id}", method=RequestMethod.PUT)
+    public void updateMessage(@PathVariable("id") String id, @RequestBody Message message) {
+        logger.info(String.format("Update a message with id: %s", id));
+    }
+    
+    @RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+    public void deleteMessage(@PathVariable("id") String id) {
+        logger.info(String.format("Delete a message with id: %s", id));
     }
 }
